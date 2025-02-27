3fa24bbbd4313e05209e50aab3670d2df96f9a9d
lindacelmaa <lindacelma98@gmail.com>
2025-02-27 09:56:17 +0200

    MyNode

--- src/datastr/MyNode.java
+++ src/datastr/MyNode.java
@@ -1,5 +1,44 @@
 package datastr;
 
-public class MyNode {
-
+public class MyNode <Ttype>{
+	private Ttype element;
+	private MyNode next = null;
+	private MyNode previous = null;
+	
+	public Ttype getElement() {
+		return element;
+	}
+	
+	public MyNode getNext() {
+		return next;
+	}
+	
+	public MyNode getPrevious() {
+		return previous;
+	}
+	
+	public void setElement(Ttype inputElement) {
+		if(inputElement != null) {
+			element = inputElement;
+		}else {
+			element = (Ttype) new Object();
+		}
+	}
+	
+	public void setNext(MyNode inputNext) {
+		next = inputNext;
+	}
+	
+	public void setPrevious(MyNode inputPrevious) {
+		previous = inputPrevious;
+	}
+	
+	public MyNode(Ttype inputElement) {
+		setElement(inputElement);
+	}
+	
+	public String toString() {
+		return element + "";
+	}
+	
 }
